be402ef05e2048f221dbaf281f203d405fa88c12
paynec4 <48925631+paynec4@users.noreply.github.com>
2022-03-09 16:08:06 -0800

    screens update

---
+++ navigation/LinkingConfiguration.ts
@@ -0,0 +1,51 @@
+/**
+ * Learn more about deep linking with React Navigation
+ * https://reactnavigation.org/docs/deep-linking
+ * https://reactnavigation.org/docs/configuring-links
+ */
+
+import { LinkingOptions } from '@react-navigation/native';
+import * as Linking from 'expo-linking';
+
+import { RootStackParamList } from '../types';
+
+const linking: LinkingOptions<RootStackParamList> = {
+  prefixes: [Linking.makeUrl('/')],
+  config: {
+    screens: {
+      Root: {
+        screens: {
+          User: {
+            screens: {
+              UserScreen: 'user',
+            },
+          },
+          Calendar: {
+            screens: {
+              CalenarScreen: 'calendar',
+            },
+          },
+          Home: {
+            screens: {
+              HomeScreen: 'home',
+            },
+          },
+          Edit: {
+            screens: {
+              EditScreen: 'edit',
+            },
+          },
+          Pdf: {
+            screens: {
+              PdfScreen: 'pdf',
+            },
+          },
+        },
+      },
+      Modal: 'modal',
+      NotFound: '*',
+    },
+  },
+};
+
+export default linking;
